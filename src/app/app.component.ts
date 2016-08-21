001b1acb9d1801d2445874f98f3c72f00b4eeeb3
ant <ant@maxant.co.uk>
2016-08-21 07:29:56 +0200

    brought up to 2.0.0-rc.5 standard with webpack. all working including service exploration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,17 @@ export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
   constructor(private heroService: HeroService) { }
+
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit() {
     this.getHeroes();
   }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
